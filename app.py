fa2ec23e10f719a97c1bb2418fbec0b487bb36f5
fateme-kz <fatemekhazaei17@gmail.com>
2024-11-24 19:28:39 +0330

    changed host

--- app.py
+++ app.py
@@ -6,4 +6,4 @@ from src import *
 from src.Users import *
 
 if __name__ == '__main__':
-    app.run(debug=True, host='192.168.10.132', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
