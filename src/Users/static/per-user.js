8311a0e819a334cdb9f45dcfe193802e74d397ee
fateme-kz <fatemekhazaei17@gmail.com>
2024-12-04 10:41:25 +0330

    added icons

---
+++ src/Users/static/per-user.js
@@ -0,0 +1,7 @@
+document.getElementById('perUserModal').addEventListener('submit', function(    ) {});  
+
+
+
+document.getElementById('closePeruser').addEventListener('click', function() {  
+    document.getElementById('modalOverLay').style.display = 'none';
+});
