0ca630d68e4927343d92c5ca8a23f11ed9271cba
fateme-kz <fatemekhazaei17@gmail.com>
2024-12-15 15:30:54 +0330

    added comments

--- src/Users/static/per-user.js
+++ src/Users/static/per-user.js
@@ -1,3 +1,5 @@
+// close button of modal
 document.getElementById('closePeruser').addEventListener('click', function() {  
     document.getElementById('modalOverLay').style.display = 'none';
+    window.location.reload(); // Reload the page
 });
