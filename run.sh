0773fb83322c0907592aff0b1c553f308e79564b
fateme-kz <fatemekhazaei17@gmail.com>
2024-12-23 17:00:00 +0330

    updated run.sh file

--- run.sh
+++ run.sh
@@ -1,8 +1,11 @@
-#!/bin/bash  
+#!/bin/bash
 
-# Activate the virtual environment  
-echo "Activating virtual environment..."  
-source venv/bin/activate  
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# Activate the virtual environment
+echo "Activating virtual environment..."
+source venv/bin/activate
 
 # Run the application  
 echo "Running the application..."  
