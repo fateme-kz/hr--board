04808b6e19f90d61e6820cb7535e0c4f121f695c
fateme-kz <fatemekhazaei17@gmail.com>
2024-12-23 16:41:49 +0330

    added content length

--- src/__init__.py
+++ src/__init__.py
@@ -1,17 +1,29 @@
 from flask import Flask
+# from flask_cors import CORS
 from flask_migrate import Migrate
 from src.Users import bp as hr_blueprint
 from src.Users.Models import  db
 from src.Users.URLs import to_persian_digits
+import os
 
 migrate = Migrate()
 
-def create_app():
+def create_app(db_url):
 
     app = Flask(__name__)
+    
+    # Enable CORS for all routes
+    # CORS(app)
 
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///employee.db'
+    app.config['MAX_CONTENT_LENGTH'] = 100 * 1024 * 1024
+    app.config['SQLALCHEMY_DATABASE_URI'] = db_url
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+    
+    # Ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path, exist_ok=True)
+    except OSError:
+        pass
 
     db.init_app(app)
     migrate.init_app(app, db)
