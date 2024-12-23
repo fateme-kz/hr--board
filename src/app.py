dedeabb2675cd888e65daa8b363b0795c76d78d7
fateme-kz <fatemekhazaei17@gmail.com>
2024-12-23 16:34:59 +0330

    got data from .env file

--- src/app.py
+++ src/app.py
@@ -1,9 +1,17 @@
 from src import create_app
+import os
 
-app = create_app()
+# Access environment variables
+flask_debug = os.getenv('FLASK_DEBUG')
+flask_run_port = os.getenv('FLASK_RUN_PORT')
+flask_run_host = os.getenv('FLASK_RUN_HOST')
+flask_db_url = os.getenv('DATABASE_PATH')
+
+app = create_app(flask_db_url)
+app.secret_key = 'secret'
 
 from src.Users import *
 from src.Users import *
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=flask_debug, host=flask_run_host, port=flask_run_port)
