87ca9fcbca59e9014b6df43f5b03c0c6ac9b8f8c
fateme-kz <fatemekhazaei17@gmail.com>
2025-02-09 10:33:40 +0330

    added JWT configs

--- src/__init__.py
+++ src/__init__.py
@@ -19,7 +19,20 @@ def create_app(db_url):
     app.config['MAX_CONTENT_LENGTH'] = 100 * 1024 * 1024
     app.config['SQLALCHEMY_DATABASE_URI'] = db_url
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['JWT_SECRET_KEY'] = 'hr--board'
+
+
+    # Ensure JWT is stored in cookies
+    app.config['JWT_SECRET_KEY'] = 'hr_board'  # Change this to a secure secret key
+    app.config['JWT_TOKEN_LOCATION'] = ['cookies']  # Read tokens from cookies, NOT headers
+    app.config['JWT_ACCESS_COOKIE_NAME'] = 'access_token_cookie'  # Cookie name
+    app.config['JWT_REFRESH_COOKIE_NAME'] = 'refresh_token_cookie'
+    app.config['JWT_COOKIE_CSRF_PROTECT'] = False  # Disable CSRF protection for now
+    app.config['JWT_ACCESS_COOKIE_PATH'] = '/'  # Make it available site-wide
+    app.config['JWT_REFRESH_COOKIE_PATH'] = '/refresh'
+
+
+
+    jwt = JWTManager(app)
     
     # Ensure the instance folder exists
     try:
